95e30e5b1f9f49c1dcb1dfe436292d62e6222025
hueie <hueieglobal@gmail.com>
2018-03-23 18:11:55 +0900

    Commit!!!

--- polarisfinder/src/main/resources/static/js/pf-angular.js
+++ polarisfinder/src/main/resources/static/js/pf-angular.js
@@ -548,6 +548,119 @@ var global_url = location.host;
 			$scope.mapinit = function(){
 				var map = new google.maps.Map(document.getElementById('map'), {
 					center : {
+						lat: -28.024, lng: 140.887 //lat : -33.8688, lng : 151.2195
+					},
+					zoom : 3,//12
+					mapTypeId : 'roadmap'
+				});
+
+				// Create the search box and link it to the UI element.
+				
+				var input = document.getElementById('pac-input');
+				var searchBox = new google.maps.places.SearchBox(input);
+				
+				
+				//map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+				// Bias the SearchBox results towards current map's viewport.
+				map.addListener('bounds_changed', function() {
+					searchBox.setBounds(map.getBounds());
+				});
+
+				
+				
+				var markerCluster = new MarkerClusterer(map, markers,
+			            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+				
+				var locations = [
+			        {lat: -31.563910, lng: 147.154312},
+			        {lat: -33.718234, lng: 150.363181},
+			        {lat: -33.727111, lng: 150.371124},
+			        {lat: -33.848588, lng: 151.209834},
+			        {lat: -33.851702, lng: 151.216968},
+			        {lat: -34.671264, lng: 150.863657},
+			        {lat: -35.304724, lng: 148.662905},
+			        {lat: -36.817685, lng: 175.699196},
+			        {lat: -36.828611, lng: 175.790222},
+			        {lat: -37.750000, lng: 145.116667},
+			        {lat: -37.759859, lng: 145.128708},
+			        {lat: -37.765015, lng: 145.133858},
+			        {lat: -37.770104, lng: 145.143299},
+			        {lat: -37.773700, lng: 145.145187},
+			        {lat: -37.774785, lng: 145.137978},
+			        {lat: -37.819616, lng: 144.968119},
+			        {lat: -38.330766, lng: 144.695692},
+			        {lat: -39.927193, lng: 175.053218},
+			        {lat: -41.330162, lng: 174.865694},
+			        {lat: -42.734358, lng: 147.439506},
+			        {lat: -42.734358, lng: 147.501315},
+			        {lat: -42.735258, lng: 147.438000},
+			        {lat: -43.999792, lng: 170.463352}
+			      ]
+				var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+				var markers = locations.map(function(location, i) {
+			          return new google.maps.Marker({
+			            position: location,
+			            label: labels[i % labels.length]
+			          });
+			        });
+				
+				// Listen for the event fired when the user selects a prediction and
+				// retrieve
+				// more details for that place.
+				searchBox.addListener('places_changed', function() {
+					var places = searchBox.getPlaces();
+					// Clear out the old markers.
+					markers.forEach(function(marker) {
+						marker.setMap(null);
+					});
+					//markers = [];
+
+					// For each place, get the icon, name and location.
+					var bounds = new google.maps.LatLngBounds();
+					places.forEach(function(place) {
+						if (!place.geometry) {
+							console.log("Returned place contains no geometry");
+							return;
+						}
+						var icon = {
+							url : place.icon,
+							size : new google.maps.Size(71, 71),
+							origin : new google.maps.Point(0, 0),
+							anchor : new google.maps.Point(17, 34),
+							scaledSize : new google.maps.Size(25, 25)
+						};
+
+						// Create a marker for each place.
+						markers.push(new google.maps.Marker({
+							map : map,
+							icon : icon,
+							title : place.name,
+							position : place.geometry.location
+						}));
+						
+						//My Source Code
+						var latitude = place.geometry.location.lat();
+						var longitude = place.geometry.location.lng();  
+						$scope.placelatitude = latitude;
+						$scope.placelongitude = longitude;
+						$scope.placename = input.value;
+						
+						if (place.geometry.viewport) {
+							// Only geocodes have viewport.
+							bounds.union(place.geometry.viewport);
+						} else {
+							bounds.extend(place.geometry.location);
+						}
+					});
+					
+					map.fitBounds(bounds);
+				});
+			}
+			
+			$scope.mapinit2 = function(){
+				var map = new google.maps.Map(document.getElementById('map'), {
+					center : {
 						lat : -33.8688,
 						lng : 151.2195
 					},
